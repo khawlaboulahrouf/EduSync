f4385636763e97ddd41c37196fb6878fd8edcb75
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-04-16 10:52:29 +0100

    createtable users

--- script.sql
+++ script.sql
@@ -9,3 +9,13 @@ CREATE TABLE roles (
 
 INSERT INTO roles (label)
 VALUES('Admin'),('Prof'),('Student');
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR (100) NOT NULL UNIQUE,
+    pasword VARCHAR(255) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
