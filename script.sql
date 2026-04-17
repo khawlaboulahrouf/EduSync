f4454615b192c876c14a33bcaeee505cfc82c99c
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-04-17 16:31:16 +0100

    add some modification into seed .sql

--- script.sql
+++ script.sql
@@ -20,25 +20,12 @@ CREATE TABLE users (
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
-INSERT INTO users (firstname, lastname ,email ,pasword ,role_id)
-VALUES
-('Khaoula' , 'Admin' ,'khawla1@gmail.com' ,'123456' ,1),
-('hamza' , 'Prof' ,'hamza2@gmail.com' ,'456789' ,2),
-('bilal' , 'student' ,'bilal3@gmail.com' ,'135790' ,3),
-('sara' , 'Student' ,'sara1@gmail.com' ,'325498' ,1);
-
-
 CREATE TABLE classes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (100) NOT NULL,
     classroom_number VARCHAR(20)
 );
 
-
-INSERT INTO classes (name , classroom_number)
-VALUES 
-('Développeur web 2026' , 'a1'),
-('Développeur web 2025' , 'b2');
 
 CREATE TABLE courses (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -49,10 +36,7 @@ CREATE TABLE courses (
      FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO courses ( title ,description ,total_hours ,user_id)
-VALUES
-('html & css' , 'introduction au web' , 30 ,2),
-('javascript' , 'programmation web' , 40 ,2);
+
 
 CREATE table students (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -64,9 +48,6 @@ CREATE table students (
     FOREIGN KEY (class_id) REFERENCES classes(id)
 );
 
-INSERT INTO students(dateOfBirth, student_number , user_id , class_id)
-VALUES
-('2000-05-10' , 'STU001' , 3 , 1);
 
 CREATE TABLE enrollments (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -81,7 +62,4 @@ CREATE TABLE enrollments (
     UNIQUE (student_id, course_id)
 );
 
-INSERT INTO enrollments (enrolled_at , status , student_id , course_id)
-VALUES 
-('2026-04-01' , 'actif' , 1 , 1),
-('2026-04-02' , 'actif' , 1 , 2);
+
---
+++ seed.sql
@@ -0,0 +1,28 @@
+INSERT INTO users (firstname, lastname ,email ,pasword ,role_id)
+VALUES
+('Khaoula' , 'Admin' ,'khawla1@gmail.com' ,'123456' ,1),
+('hamza' , 'Prof' ,'hamza2@gmail.com' ,'456789' ,2),
+('bilal' , 'student' ,'bilal3@gmail.com' ,'135790' ,3),
+('sara' , 'Student' ,'sara1@gmail.com' ,'325498' ,1);
+
+
+INSERT INTO classes (name , classroom_number)
+VALUES 
+('Développeur web 2026' , 'a1'),
+('Développeur web 2025' , 'b2');
+
+INSERT INTO courses ( title ,description ,total_hours ,user_id)
+VALUES
+('html & css' , 'introduction au web' , 30 ,2),
+('javascript' , 'programmation web' , 40 ,2);
+
+
+INSERT INTO students(dateOfBirth, student_number , user_id , class_id)
+VALUES
+('2000-05-10' , 'STU001' , 3 , 1);
+
+
+INSERT INTO enrollments (enrolled_at , status , student_id , course_id)
+VALUES 
+('2026-04-01' , 'actif' , 1 , 1),
+('2026-04-02' , 'actif' , 1 , 2);
